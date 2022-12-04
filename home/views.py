fdae4de903822cb1e6aa7b4165f3be6d7291f47e
Sahil <prasadsahil06@gmail.com>
2022-12-05 01:43:36 +0530

    Homepage

--- home/views.py
+++ home/views.py
@@ -1,24 +1,29 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.contrib import messages
 from .models import Contact
+from blog.models import Post
+
 
 def home(request):
-    return render(request,'home/home.html')
+    allPosts = Post.objects.all()
+    context = {'allPosts': allPosts}
+    return render(request, 'home/home.html', context)
+
 
 def about(request):
-    return render(request,'home/about.html')
+    return render(request, 'home/about.html')
+
 
 def contact(request):
-    if request.method=="POST":
+    if request.method == "POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         phone = request.POST.get('phone', '')
-        dec= request.POST.get('dec', '')
-        if len(name)<3 or len(email)<10 or len(phone)<10 or len(dec)<10:
+        dec = request.POST.get('dec', '')
+        if len(name) < 3 or len(email) < 10 or len(phone) < 10 or len(dec) < 10:
             messages.error(request, 'Please enter a valid information')
         else:
             contact = Contact(name=name, email=email, phone=phone, dec=dec)
-            contact.save()    
+            contact.save()
             messages.success(request, 'Your complaint sent successfully')
-    return render(request,'home/contact.html')
-
+    return render(request, 'home/contact.html')
