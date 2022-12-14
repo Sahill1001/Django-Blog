9b3679b7fb20aa7fb825df38dcb70b48250090db
Sahil <prasadsahil06@gmail.com>
2022-12-14 20:54:39 +0530

    useradd

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='About'),
     path('contact/', views.contact, name='Contact'),
     path('search/', views.search, name='Contact'),
+    path('signup/', views.handleUser, name='HandleUser'),
+    path('login/', views.handleLogin, name='HandleLogin'),
+    path('logout/', views.handleLogout, name='HandleLogout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
 from django.contrib import messages
 from .models import Contact
 from blog.models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 
 def home(request):
@@ -45,3 +47,56 @@ def contact(request):
             contact.save()
             messages.success(request, 'Your complaint sent successfully')
     return render(request, 'home/contact.html')
+
+
+
+def handleUser(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+        
+        if not username.isalnum():
+            messages.error(request, " User name should only contain letters and numbers")
+            return redirect('Home')
+        if (pass1!= pass2):
+             messages.error(request, " Passwords do not match")
+             return redirect('Home')
+         
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name= fname
+        myuser.last_name= lname
+        myuser.save()
+        messages.success(request, " Your iCoder account has been successfully created")
+        return redirect('Home')
+    
+    else:
+        return HttpResponse("404 - Not found")
+    
+    
+    
+def handleLogin(request):
+    if request.method=="POST":
+        username = request.POST['loginusername']
+        password = request.POST['loginpass']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            messages.success(request,"You have been logged in successfully.")
+            return redirect("Home")
+        else:
+            messages.error(request,"Invalid credentials. please try again.")
+            return redirect("Home")
+    else:
+        return HttpResponse("404- Not Found")
+    
+    
+def handleLogout(request):
+    logout(request)
+    messages.success(request,"Logout successfully")
+    return redirect("Home")
+    
+    
