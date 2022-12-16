e9bbddfc37b4cfae0ec84b78948a807edb64a8f1
Sahil <prasadsahil06@gmail.com>
2022-12-16 18:53:45 +0530

    logUser

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     srn=models.BigAutoField(primary_key=True)
-    title= models.CharField(max_length=30)
+    title= models.CharField(max_length=100)
     content=models.TextField()
-    author= models.CharField(max_length=10)
-    slug= models.CharField(max_length=10,blank=True)
+    author= models.CharField(max_length=20)
+    slug= models.CharField(max_length=50,blank=True)
     image=models.ImageField(upload_to='static/blog',blank=True)
     date=models.DateTimeField(blank=True)
     
